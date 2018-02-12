0b8fcddc19ad72d0a5cd94c8ccdc7bb26246495c
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-02-11 19:16:21 -0500

    Add player prefab

---
+++ src/prefabs/Player.ts
@@ -0,0 +1,26 @@
+import { Game } from '../states/Game';
+
+export interface IPlayerData {
+  attack: number;
+  defense: number;
+  gold: number;
+  health: number;
+  items: any[];
+  quests: any[];
+}
+
+export class Player extends Phaser.Sprite {
+  public data: IPlayerData;
+  private state: Game;
+
+  constructor(state: Game, x: number, y: number, data: IPlayerData) {
+    super(state.game, x, y, 'player');
+
+    this.data = data;
+    this.state = state;
+
+    this.anchor.setTo(0.5);
+    this.animations.add('walking', [0, 1, 0], 6, false);
+    this.game.physics.arcade.enable(this);
+  }
+}
--- src/states/Game.ts
+++ src/states/Game.ts
@@ -1,9 +1,12 @@
+import { IPlayerData, Player } from '../prefabs/Player';
+
 export class Game extends Phaser.State {
   private backgroundLayer: Phaser.TilemapLayer;
   private collisionLayer: Phaser.TilemapLayer;
   private currentLevel: string;
   private cursors: Phaser.CursorKeys;
   private map: Phaser.Tilemap;
+  private player: Player;
   private readonly PLAYER_SPEED = 90;
 
   public init(currentLevel: string) {
@@ -28,6 +31,19 @@ export class Game extends Phaser.State {
     this.world.sendToBack(this.backgroundLayer);
     this.map.setCollisionBetween(1, 16, true, 'collisionLayer');
     this.collisionLayer.resizeWorld();
+
+    const playerData: IPlayerData = {
+      attack: 12,
+      defense: 8,
+      gold: 100,
+      health: 25,
+      items: [],
+      quests: [],
+    };
+
+    this.player = new Player(this, 100, 100, playerData);
+
+    this.add.existing(this.player);
   }
 
   private gameOver() {
